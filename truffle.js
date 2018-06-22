4b5552a0290276dc3729bba43d22d1b7449991cb
sidharth <sidharthmalhotra0094@gmail.com>
2018-06-21 20:32:36 -0400

    [TASK] Added truffle.js file

---
+++ truffle.js
@@ -0,0 +1,21 @@
+let HDWalletProvider = require("truffle-hdwallet-provider");
+
+module.exports = {
+  solc: {
+    optimizer: {
+      // disabled by default
+      enabled: true,
+      // Optimize for how many times you intend to run the code.
+      // Lower values will optimize more for initial deployment cost, 
+      // higher values will optimize more for high-frequency usage.
+      runs: 500
+    },
+  },
+  networks: {
+    development: {
+      host: "127.0.0.1",
+      port: 7545,
+      network_id: "*" // Match any network id
+    }
+  }
+};
