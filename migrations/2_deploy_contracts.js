030a0e230dac726eecdbd521d47fffece0e4f390
sidharth <sidharthmalhotra0094@gmail.com>
2018-07-18 10:14:32 -0400

    [WIP] Updated deploy script

--- migrations/2_deploy_contracts.js
+++ migrations/2_deploy_contracts.js
@@ -2,21 +2,32 @@
  * This file deploys all the compiled smart contracts
  */
 let Blockvitae = artifacts.require("./Blockvitae.sol");
+let BlockvitaeInsert = artifacts.require("./BlockvitaeInsert.sol");
+let BlockvitaeGetter = artifacts.require("./BlockvitaeGetter.sol");
+let BlockvitaeDelete = artifacts.require("./BlockvitaeDelete.sol");
 let User = artifacts.require("./User.sol");
-let DB = artifacts.require("./DB.sol");
+let Db = artifacts.require("./DB.sol");
+let DbGetter = artifacts.require("./DbGetter.sol");
+let DbDelete = artifacts.require("./DbDelete.sol");
+let DbInsert = artifacts.require("./DbInsert.sol");
 
- module.exports = function (deployer) {
-     // deploy contracts
-      deployer
+module.exports = function (deployer) {
+    // deploy contracts
+    deployer
         .deploy(User)
         .then(() => {
-            deployer.link(User, [DB, Blockvitae]);
+            deployer.link(User, [Db, Blockvitae]);
             // return
             return deployer
-                    .deploy(DB)
-                    .then(() => {
-                        // return
-                        return deployer.deploy(Blockvitae, DB.address);
-                    });
+                .deploy([DbGetter, DbDelete, DbInsert])
+                .then(() => {
+                   return deployer.deploy(Blockvitae, DbInsert.address, DbGetter.address, DbDelete.address)
+                   .then(() => {
+                    return deployer.deploy([
+                        [BlockvitaeGetter, DbInsert.address, DbGetter.address, DbDelete.address],
+                        [BlockvitaeDelete, DbInsert.address, DbGetter.address, DbDelete.address],
+                        [BlockvitaeInsert, DbInsert.address, DbGetter.address, DbDelete.address]]);
+                   })
+                });
         });
- }
+}
