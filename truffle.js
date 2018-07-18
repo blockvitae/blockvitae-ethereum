d30cd2b8051a45bce6ee585ecaa33ba482bf72c9
sidharth <sidharthmalhotra0094@gmail.com>
2018-07-18 10:21:42 -0400

    [WIP] Updated truffle.js

--- truffle.js
+++ truffle.js
@@ -8,14 +8,20 @@ module.exports = {
       // Optimize for how many times you intend to run the code.
       // Lower values will optimize more for initial deployment cost, 
       // higher values will optimize more for high-frequency usage.
-      runs: 500
+      runs: 600
     },
   },
   networks: {
     development: {
       host: "127.0.0.1",
       port: 7545,
-      network_id: "*" // Match any network id
+      network_id: "*", // Match any network id
+      gas: 2700000
+    },
+    ropsten: {
+      provider: new HDWalletProvider(mnemonics, infura), // Infura Node
+      network_id: 3,
+      gas: 2700000
     }
   }
 };
