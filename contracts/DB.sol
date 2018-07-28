2a0f4c318970767daf9d92b13d3f382ff4f8ee50
sidharth <sidharthmalhotra0094@gmail.com>
2018-07-28 00:13:42 -0400

    [WIP] Updated DB.sol tp support caller address as parameter in modifier. Also added API for accessing mapping and variables from other contracts

--- contracts/DB.sol
+++ contracts/DB.sol
@@ -40,13 +40,13 @@ contract DB {
     // get deployed DbReset sets it to false to avoid any public access
     // Blockvitae Contracts becomes owner of
     // this contract
-    modifier isOwner() {
-        require(owners[msg.sender] || owners[address(this)]);
+    modifier isOwner(address _sender) {
+        require(owners[_sender] || owners[address(this)]);
         _;
     }
 
-    function isOwnerDB() public view isOwner returns(bool){
-        return (owners[msg.sender] || owners[address(this)]);
+    function isOwnerDB(address _sender) public view isOwner(_sender) returns(bool){
+        return (owners[_sender] || owners[address(this)]);
     }
 
     // @description
@@ -54,7 +54,7 @@ contract DB {
     //
     // @param address _blockvitae
     // address of the Blockvitae contract
-    function setOwner(address _blockvitae) public isOwner{
+    function setOwner(address _blockvitae) public isOwner(_blockvitae){
         owners[_blockvitae] = true;
     }
 
@@ -63,88 +63,104 @@ contract DB {
     //
     // @param address _blockvitae
     // address of the Blockvitae contract
-    function removeOwner(address _blockvitae) public isOwner{
+    function removeOwner(address _blockvitae, address _sender) public isOwner(_sender){
         owners[_blockvitae] = false;
     }
 
-    function getUserDetail(address _user) public view isOwner returns(User.UserDetail){
+    function getUserDetail(address _user, address _sender) public view isOwner(_sender) returns(User.UserDetail){
         return users[_user].personal;
     }
 
-    function setUserDetail(User.UserDetail _personal, address _user) public isOwner {
+    function setUserDetail(User.UserDetail _personal, address _user, address _sender) public isOwner(_sender) {
         users[_user].personal = _personal;
     }
 
-    function getUserNameAddr(bytes32 _user) public view isOwner returns(address) {
+    function getUserNameAddr(bytes32 _user, address _sender) public view isOwner(_sender) returns(address) {
         return userNames[_user];
     }
 
-    function setUserNameAddr(bytes32 _userName, address _user) public isOwner {
+    function setUserNameAddr(bytes32 _userName, address _user, address _sender) public isOwner(_sender) {
         userNames[_userName] = _user;
     }
 
-    function getUserSocial(address _user) public view isOwner returns(User.UserSocial) {
+    function getUserSocial(address _user, address _sender) public view isOwner(_sender) returns(User.UserSocial) {
         return users[_user].social;
     }
 
-    function setUserSocial(User.UserSocial _social, address _user) public isOwner {
+    function setUserSocial(User.UserSocial _social, address _user, address _sender) public isOwner(_sender) {
         users[_user].social = _social;
     }
 
-    function getUserProject(address _user) public view isOwner returns(User.UserProject[]) {
+    function getUserProject(address _user, address _sender) public view isOwner(_sender) returns(User.UserProject[]) {
         return users[_user].projects;
     }
 
-    function setUserProject(User.UserProject _project, address _user) public isOwner {
+    function setUserProject(User.UserProject _project, address _user, address _sender) public isOwner(_sender) {
         users[_user].projects.push(_project);
     }
 
-    function getUserWorkExp(address _user) public view isOwner returns(User.UserWorkExp[]) {
+    function deleteUserProject(uint _index, address _user, address _sender) public isOwner(_sender) {
+        users[_user].projects[_index].isDeleted = true;
+    }
+
+    function getUserWorkExp(address _user, address _sender) public view isOwner(_sender) returns(User.UserWorkExp[]) {
         return users[_user].work;
     }
 
-    function setUserWorkExp(User.UserWorkExp _work, address _user) public isOwner {
+    function setUserWorkExp(User.UserWorkExp _work, address _user, address _sender) public isOwner(_sender) {
         users[_user].work.push(_work);
     }
 
-    function getUserSkill(address _user) public view isOwner returns(User.UserSkill) {
+    function deleteUserWorkExp(uint _index, address _user, address _sender) public isOwner(_sender) {
+        users[_user].work[_index].isDeleted = true;
+    }
+
+    function getUserSkill(address _user, address _sender) public view isOwner(_sender) returns(User.UserSkill) {
         return users[_user].skills;
     }
 
-    function setUserSkills(User.UserSkill _skills, address _user) public isOwner {
+    function setUserSkills(User.UserSkill _skills, address _user, address _sender) public isOwner(_sender) {
         users[_user].skills = _skills;
     }
 
-    function getUserIntro(address _user) public view isOwner returns(User.UserIntroduction) {
+    function getUserIntro(address _user, address _sender) public view isOwner(_sender) returns(User.UserIntroduction) {
         return users[_user].introduction;
     }
 
-    function setUserIntro(User.UserIntroduction _intro, address _user) public isOwner {
+    function setUserIntro(User.UserIntroduction _intro, address _user, address _sender) public isOwner(_sender) {
         users[_user].introduction = _intro;
     }
 
-    function getUserEducation(address _user) public view isOwner returns(User.UserEducation[]) {
+    function getUserEducation(address _user, address _sender) public view isOwner(_sender) returns(User.UserEducation[]) {
         return users[_user].education;
     }
 
-    function setUserEducation(User.UserEducation _edu, address _user) public isOwner {
+    function setUserEducation(User.UserEducation _edu, address _user, address _sender) public isOwner(_sender) {
         users[_user].education.push(_edu);
     }
 
-    function deleteUserEducation(uint _index, address _user) public isOwner {
+    function deleteUserEducation(uint _index, address _user, address _sender) public isOwner(_sender) {
         users[_user].education[_index].isDeleted = true;
     }
 
-    function getUserPublication(address _user) public view isOwner returns(User.UserPublication[]) {
+    function getUserPublication(address _user, address _sender) public view isOwner(_sender) returns(User.UserPublication[]) {
         return users[_user].publications;
     }
 
-    function setUserPublication(User.UserPublication _pub, address _user) public isOwner {
+    function setUserPublication(User.UserPublication _pub, address _user, address _sender) public isOwner(_sender) {
         users[_user].publications.push(_pub);
     }
 
-    function isExists(address _user) public view isOwner returns(bool) {
+    function deleteUserPublication(uint _index, address _user, address _sender) public isOwner(_sender) {
+        users[_user].publications[_index].isDeleted = true;
+    }
+
+    function isExists(address _user, address _sender) public view isOwner(_sender) returns(bool) {
         return users[_user].exists;
+    }
+
+    function incrementTotalUsers(address _sender) public isOwner(_sender) {
+        totalUsers++;
     }
 
     // @description
@@ -152,7 +168,7 @@ contract DB {
     // 
     // @param address _user
     // address of the user
-    function persistUser(address _user) public {
+    function persistUser(address _user, address _sender) public isOwner(_sender) {
         users[_user].exists = true;
         users[_user].owner = _user;
     }
